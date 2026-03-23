60d88c72129621f20041f0762216a943a7563541
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-03-23 10:03:49 -0600

    playlist con ll doblemente enlazada

--- ll.py
+++ ll.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None  #double
 
     def __repr__(self):
         return f"| {self.data['nombre']} |"
@@ -10,7 +11,7 @@ class LinkedList:
     def __init__(self):
         self.start = None
         self.current = None
-        self.length = 0  #llevar la cuenta para el shuffle más adelante
+        self.length = 0
 
     def append(self, data):
         new_node = Node(data)
@@ -25,4 +26,16 @@ class LinkedList:
         while actual.next is not None:
             actual = actual.next
         
+        #double
         actual.next = new_node
+        new_node.prev = actual
+
+    def next_song(self):
+        if self.current is not None and self.current.next is not None:
+            self.current = self.current.next
+        return self.current
+
+    def prev_song(self):
+        if self.current is not None and self.current.prev is not None:
+            self.current = self.current.prev
+        return self.current
