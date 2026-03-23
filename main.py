8970a6366cd77b6618db479e237de55e61ec15ee
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-03-23 10:23:40 -0600

    Implementación de profiling

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
-
+# main.py
 from ll import LinkedList
 from songs_data import obtener_100_canciones
+from memory_profiler import profile
+import timeit
 
-def cargar_playlist(playlist):
-    #1. Obtiene la data del archivo separado
+
+# 1. PERFILAMIENTO DE MEMORIA
+@profile
+def perfilar_carga_memoria():
+    playlist_memoria = LinkedList()
     datos = obtener_100_canciones()
     
-    #2. Inserta de forma iterativa
+    # Carga iterativa
     for cancion in datos:
-        playlist.append(cancion)
+        playlist_memoria.append(cancion)
+        
+    return "Carga de memoria completada"
+
+# 2.PERFILAMIENTO DE TIEMPO
+
+def perfilar_carga_tiempo():
+    playlist_tiempo = LinkedList()
+    datos = obtener_100_canciones()
+    
+    #Carga iterativa
+    for cancion in datos:
+        playlist_tiempo.append(cancion)
 
 if __name__ == "__main__":
-    mi_playlist = LinkedList()
-    print("Playlist creada (vacía)")
+    print("="*50)
+    print(" INICIANDO PERFILAMIENTO DE MEMORIA ")
+    print("="*50)
+    #Al llamar esta función, el decorador @profile imprimirá la tabla
+    perfilar_carga_memoria()
     
-    # Llenamos la playlist
-    cargar_playlist(mi_playlist)
-    
-    print("¡Se han cargado 100 canciones!")
-    
-    if mi_playlist.current is not None:
-        print(f"Primera canción en lista: {mi_playlist.current.data['nombre']}")
+    print("\n" + "="*50)
+    print(" INICIANDO PERFILAMIENTO DE TIEMPO ")
+    print("="*50)
+    #Usa timeit para correr la carga 500 veces y promediar
+    tiempo_total = timeit.timeit(perfilar_carga_tiempo, number=500)
+    print(f"El tiempo total para 500 ejecuciones fue: {tiempo_total:.5f} segundos")
+    print(f"El tiempo promedio por ejecucion (100 canciones) fue: {tiempo_total / 500:.6f} segundos")
