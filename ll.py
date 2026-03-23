1f9d7736c708598bcd9326510b5778f29e471705
Paula Sofia Bilbao Cruz <sofia.bilbao@ufm.edu>
2026-03-23 10:28:41 -0600

    Shuffle añadido en ll

--- ll.py
+++ ll.py
@@ -1,8 +1,10 @@
+import random
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        self.prev = None  #double
+        self.prev = None
 
     def __repr__(self):
         return f"| {self.data['nombre']} |"
@@ -11,7 +13,9 @@ class LinkedList:
     def __init__(self):
         self.start = None
         self.current = None
-        self.length = 0
+        self.length = 0          # Para saber hasta dónde puede saltar
+        self.is_shuffle = False  # Estado del reproductor
+        self.history = []        # Historial para el botón de "Anterior" en shuffle
 
     def append(self, data):
         new_node = Node(data)
@@ -26,16 +30,50 @@ class LinkedList:
         while actual.next is not None:
             actual = actual.next
         
-        #double
         actual.next = new_node
         new_node.prev = actual
 
+    def toggle_shuffle(self):
+        self.is_shuffle = not self.is_shuffle
+        if self.is_shuffle:
+            # Limpia el historial al inciarlo para empezar limpio
+            self.history = []
+
     def next_song(self):
-        if self.current is not None and self.current.next is not None:
-            self.current = self.current.next
+        if self.current is None:
+            return None
+
+        if self.is_shuffle:
+            # 1. Guarda dónde está antes de saltar
+            self.history.append(self.current)
+            
+            # 2. Elige cuántos pasos dara al azar
+            pasos = random.randint(0, self.length - 1)
+            
+            # 3. Navegación basada en nodos
+            actual = self.start
+            for _ in range(pasos):
+                actual = actual.next
+            
+            self.current = actual
+        else:
+            # Comportamiento normal (lineal)
+            if self.current.next is not None:
+                self.current = self.current.next
         return self.current
 
     def prev_song(self):
-        if self.current is not None and self.current.prev is not None:
-            self.current = self.current.prev
+        if self.current is None:
+            return None
+
+        if self.is_shuffle:
+            # Retrocede sacando el último nodo visitado del historial
+            if len(self.history) > 0:
+                self.current = self.history.pop()
+            else:
+                print("\n[!] Ya estás en la primera canción del shuffle.")
+        else:
+            # Comportamiento normal (lineal)
+            if self.current.prev is not None:
+                self.current = self.current.prev
         return self.current
